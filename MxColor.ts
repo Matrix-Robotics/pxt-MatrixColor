537c49875be0b39bcc9283f9e18df550cf4bc796
DCisHurt <roger33669@gmail.com>
2021-08-16 09:53:17 +0800

    fix color detection

--- MxColor.ts
+++ MxColor.ts
@@ -21,7 +21,7 @@ enum ColorType
     K
 }
 
-//% weight=0 color=#000016 icon="\uf030" block="MxColor"
+//% weight=9 color=#000016 icon="\uf030" block="MxColor"
 namespace MxColor{
 
     const MxColor_ADDR = 0x22
@@ -49,7 +49,7 @@ namespace MxColor{
      *@param lmode [0-1] set motor speed; eg: 0, 1
     */
     //%block="start up the color sensor|gamma correnction is %gamma|fill-light is %light and set in mode:%lmode"
-    //%weight=994 inlineInputMode="external" %blockID="MxColor_init"
+    //%weight=194 inlineInputMode="external" %blockID="MxColor_init"
     export function init(gamma: ColorLogic, light: ColorLogic, lmode: ColorLightMode): void {
         let setting = 0x08
         setting = (gamma) ? setting + 4 : setting
@@ -68,7 +68,7 @@ namespace MxColor{
      *@param pwm [0-255] set motor speed; eg: 0, 90
     */
     //%block="set fill light brightness at pwm |%pwm|"
-    //%weight=993 %blockID="MxColor_light"
+    //%weight=193 %blockID="MxColor_light"
     //% pwm.min=0 pwm.max=255
     export function setLight(pwm: number): void {
         i2cWrite(ColorReg.Device_LIGHT, pwm)
@@ -80,84 +80,39 @@ namespace MxColor{
      *@param color [4-10] choose color; eg: 4, 6
     */
     //%block="read |%color| from sensor"
-    //%weight=96 %blockID="MxColor_color"
+    //%weight=192 %blockID="MxColor_color"
     export function readColor(color: ColorType): number {
         let reg = 0
         reg += <number>color
         return i2cRead(reg)
     }
 
-
     /**
      *read grayscale from sensor
     */
     //%block="read grayscale from sensor"
-    //%weight=95 %blockID="MxColor_gray"
+    //%weight=191 %blockID="MxColor_gray"
     export function readGrayscale(): number {
         return i2cRead(ColorReg.Device_GRAY)
     }
     
     /**
-     * read color string from sensor
+     * Checks the color is being detected
+     * @param color the color to detect
     */
-    //%block="read color string from sensor"
-    //%weight=94 %blockID="MxColor_number"
-    export function readColorNumber(): number {
-        let num = i2cRead(ColorReg.Device_NUM_COLOR)
-        let cmykDec = 0
-        switch(num){
-            case 0:
-                cmykDec = 0        //black
-                break
-            case 1:
-                cmykDec = 16777215 //white
-                break
-            case 2:
-                cmykDec = 65535    //Cyan
-                break
-            case 3:
-                cmykDec = 33023    //Ocean
-                break
-            case 4:
-                cmykDec = 255      //blue
-                break
-            case 5:
-                cmykDec = 8388863  //Violet
-                break
-            case 6:
-                cmykDec = 16711935 //Magenta
-                break
-            case 7:
-                cmykDec = 16711808 //Raspberry
-                break
-            case 8:
-                cmykDec = 16711680 //Red
-                break
-            case 9:
-                cmykDec = 16744448 //Orange
-                break
-            case 10:
-                cmykDec = 16776960 //Yellow
-                break
-            case 11:
-                cmykDec = 8453888  //SpringGreen
-                break
-            case 12:
-                cmykDec = 65280    //Green
-                break
-            case 13:
-                cmykDec = 65408    //Turquoise
-                break
-            default:
-                break
-        }
-        return cmykDec
+    //% block="is **color sensor** |detected|%color=colorPicker"
+    //% blockId=colorisColorDetectedDetected
+    //% blockNamespace=MxColor
+    //% weight=99
+    export function isColorDetected(color: number): boolean {
+        let logic = (readColorNumber() == color) ? true : false
+        return logic;
     }
 
     /**
      * A color enum picker
-     * @param color to use, converted to dec, eg: 8388863  //Violet
-     */
+     * @param color to use, converted to dec, eg: 0x8000FF  //Violet
+    */
     //% blockId=colorPicker block="%color" shim=TD_ID
     //% weight=0 blockHidden=1 turnRatio.fieldOptions.decompileLiterals=1
     //% color.fieldEditor="colornumber"
@@ -167,29 +122,56 @@ namespace MxColor{
         return color;
     }
 
-    /**
-     * Checks the color is being detected
-     * @param color the color to detect
-     */
-    //% block="is **color sensor** |detected|%color=colorPicker"
-    //% blockId=colorisColorDetectedDetected
-    //% blockNamespace=MxColor
-    //% weight=99
-    export function isColorDetected(color: number) {
-        return this.readColorNumber() == color;
-    }
-
-
-    /**
-     * Returns a color that the sensor can detect
-     * @param color the color sensed by the sensor, eg: 8388863  //Violet
-     */
-    //% shim=TD_ID
-    //% blockId=colorSensorColor
-    //% block="color %color=colorPicker"
-    //% weight=971
-    export function color(color: number): number {
-        return color;
+    function readColorNumber(): number {
+        let num = i2cRead(ColorReg.Device_NUM_COLOR)
+        let cmykDec = 0
+        switch(num){
+            case 0:
+                cmykDec = 0x000000  //black
+                break
+            case 1:
+                cmykDec = 0xFFFFFF  //white
+                break
+            case 2:
+                cmykDec = 0x00FFFF  //Cyan
+                break
+            case 3:
+                cmykDec = 0x0080FF  //Ocean
+                break
+            case 4:
+                cmykDec = 0x0000FF  //blue
+                break
+            case 5:
+                cmykDec = 0x8000FF  //Violet
+                break
+            case 6:
+                cmykDec = 0xFF00FF  //Magenta
+                break
+            case 7:
+                cmykDec = 0xFF0080  //Raspberry
+                break
+            case 8:
+                cmykDec = 0xFF0000  //Red
+                break
+            case 9:
+                cmykDec = 0xFF8000  //Orange
+                break
+            case 10:
+                cmykDec = 0xFFFF00  //Yellow
+                break
+            case 11:
+                cmykDec = 0x80FF00  //SpringGreen
+                break
+            case 12:
+                cmykDec = 0X00FF00  //Green
+                break
+            case 13:
+                cmykDec = 0X00FF80  //Turquoise
+                break
+            default:
+                break
+        }
+        return cmykDec
     }
 
     function i2cWrite(reg: number, value: number): void {
@@ -202,5 +184,5 @@ namespace MxColor{
     function i2cRead(reg: number): number {
         pins.i2cWriteNumber(MxColor_ADDR, reg, NumberFormat.UInt8LE)
         return pins.i2cReadNumber(MxColor_ADDR, NumberFormat.UInt8LE, false)
-    } 
+    }
 }
