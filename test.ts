7bcb0c118eb595cdf90a1fd30396afd46f1573d4
DCisHurt <roger33669@gmail.com>
2021-08-16 09:52:37 +0800

    update test

--- test.ts
+++ test.ts
@@ -1,4 +1,22 @@
-MxColor.init(1, 1, ColorLightMode.PWM)
+MxColor.init(ColorLogic.Open, ColorLogic.Open, ColorLightMode.PWM)
+MxColor.setLight(200)
 basic.forever(function () {
-    serial.writeLine(MxColor.readColorNumber())
+    serial.writeString("R=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.R)))
+    serial.writeString("G=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.G)))
+    serial.writeString("B=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.B)))
+    serial.writeString("C=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.C)))
+    serial.writeString("M=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.M)))
+    serial.writeString("Y=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.Y)))
+    serial.writeString("K=")
+    serial.writeLine("" + (MxColor.readColor(ColorType.K)))
+    if (MxColor.isColorDetected(0xFFFF00)) {
+        serial.writeLine("Yellow!")
+    }
 })
+    
